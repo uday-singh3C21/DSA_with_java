8eb93042557f1ee29f1a102ca8c7ff9cfada542d
udaysingh_3c21 <112183593+uday-singh3C21@users.noreply.github.com>
2024-04-18 23:52:33 +0530

    Add files via upload

---
+++ variable_in_java.java
@@ -0,0 +1,11 @@
+public class variable_in_java {
+    public static void main(String[] args){
+        //variabe
+        String name="uday singh";
+        int age=19;
+        int a=1;
+        int b=3;
+        int sum=a+b;
+        System.out.println(sum);
+    }
+}
