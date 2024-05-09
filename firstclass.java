21723491a856b14909d22b955debec99272e40fe
udaysingh_3c21 <112183593+uday-singh3C21@users.noreply.github.com>
2024-05-10 00:43:03 +0530

    Add files via upload

---
+++ firstclass.java
@@ -0,0 +1,12 @@
+/*public class firstclass {
+    public static void main(String[] args){
+        //output
+        System.out.println("hello world");
+    }
+}*/
+
+public class firstclass{
+    public static void main(String[] args){
+        System.out.print("*\n**\n***\n****");
+    }
+}
